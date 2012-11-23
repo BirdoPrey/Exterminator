00425f13582813b701717c7c001c6a9d92ba2fe5
Daniel <BirdofPrey160@gmail.com>
2012-11-23 16:27:27 -0500

    First Commit!
    Main application class and .gitignore file created!

---
+++ Exterminator/src/birdoprey/exterminator/ExterminatorApp.java
@@ -0,0 +1,14 @@
+package birdoprey.exterminator;
+
+//Main application class - a most auspicious start
+public class ExterminatorApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
